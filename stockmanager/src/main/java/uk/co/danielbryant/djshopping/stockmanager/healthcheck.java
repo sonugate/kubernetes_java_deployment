55871404c82cc086e4fb12dee8a313921e7d84bb
sonugate <49272572+sonugate@users.noreply.github.com>
2023-12-14 01:41:53 +0530

    Update healthcheck.java

--- stockmanager/src/main/java/uk/co/danielbryant/djshopping/stockmanager/healthcheck.java
+++ stockmanager/src/main/java/uk/co/danielbryant/djshopping/stockmanager/healthcheck.java
@@ -11,3 +11,16 @@
 //         return "Hello User !! " + new Date();
 //     }
 // }
+package uk.co.danielbryant.djshopping.stockmanager;
+ 
+import java.util.Date;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+ 
+@RestController
+public class healthcheck {
+    @GetMapping("/actuator/health")
+    public String healthcheck() {
+        return "Hello User !! " + new Date();
+    }
+}
